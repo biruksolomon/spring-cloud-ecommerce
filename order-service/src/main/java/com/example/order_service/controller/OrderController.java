db99bcca6f6ae9d8a0368494c16eb360f1d6bcc1
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-07 15:17:25 +0300

    adding controller in order servie package

---
+++ order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.example.order_service.controller;
+
+
+import com.example.order_service.domin.Order;
+import com.example.order_service.service.OrderService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public Order createorder(@RequestBody Order order){
+
+        return orderService.createOrder(order);
+    }
+}
--- order-service/src/main/java/com/example/order_service/domin/Order.java
+++ order-service/src/main/java/com/example/order_service/domin/Order.java
@@ -5,17 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "orders")
-@Builder
-@RequiredArgsConstructor
 public class Order {
     @Id
     @GeneratedValue
