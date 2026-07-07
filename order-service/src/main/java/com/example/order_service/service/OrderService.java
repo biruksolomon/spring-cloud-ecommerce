e1137e16a21904cfc6e36369a293784c054e9f6d
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-07 22:41:30 +0300

    shift RestTemplate to FeignClient

--- order-service/src/main/java/com/example/order_service/service/OrderService.java
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -1,9 +1,11 @@
 package com.example.order_service.service;
 
 
+import com.example.order_service.client.ProductClient;
 import com.example.order_service.domin.Order;
 import com.example.order_service.dtos.ProductResponseDto;
 import com.example.order_service.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,20 +13,24 @@ import org.springframework.web.client.RestTemplate;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final RestTemplate restTemp;
+//    private final RestTemplate restTemp;
+    @Autowired
+    private ProductClient productClient;
 
     public OrderService(
             OrderRepository orderRepository,
             RestTemplate restTemp) {
         this.orderRepository = orderRepository;
-        this.restTemp= restTemp;
+//        this.restTemp= restTemp;
     }
 
     public Order createOrder(Order order){
 
-        ProductResponseDto productResponseDto = restTemp.getForObject(
+       /* ProductResponseDto productResponseDto = restTemp.getForObject(
                 "http://localhost:8081/products/"+order.getProductId(),ProductResponseDto.class
-        );
+        );*/
+
+        ProductResponseDto productResponseDto = productClient.getProduct(order.getProductId());
 //        order.builder().totalPrice(productResponseDto.getPrice()* order.getQuantity());
         assert productResponseDto != null;
         order.setTotalPrice(productResponseDto.getPrice()* order.getQuantity());
