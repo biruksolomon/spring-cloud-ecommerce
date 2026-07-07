e717a0e7cc7b12f0ebb198c0318ca1178c8572a9
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-07 23:07:03 +0300

    remove hardcode url and handled by eureka in FeignClient

--- order-service/src/main/java/com/example/order_service/client/ProductClient.java
+++ order-service/src/main/java/com/example/order_service/client/ProductClient.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-        name = "product-service",
-        url = "http://localhost:8081"
+        name = "product-service"
+//        , url = "http://localhost:8081"
 )
 public interface ProductClient {
 
