1c294d2fba48ad6bea536815422fa7475a46190e
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-08 19:52:58 +0300

    implement test order consumer to test rabbit listener and regsier notification:queue

---
+++ order-service/src/main/java/com/example/order_service/consumer/OrderConsumer.java
@@ -0,0 +1,30 @@
+package com.example.order_service.consumer;
+
+import com.example.order_service.config.RabbitMQConfig;
+import com.example.order_service.event.OrderCreatedEvent;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+
+/*
+*
+*   Testing as a Consumer
+*
+* */
+@Component
+public class OrderConsumer {
+
+    @RabbitListener(
+            queues = RabbitMQConfig.QUEUE
+    )
+    public void receive(
+            OrderCreatedEvent event){
+
+        System.out.println(
+                "Received Order : "
+                        + event.getOrderId()
+        );
+
+    }
+
+}
