d54591d9402b409e31f61dab5b848989920c09c5
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-08 08:16:31 +0300

    add ciruitbreaker annotaion and impl fallbackmethod

--- order-service/src/main/java/com/example/order_service/service/OrderService.java
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -5,6 +5,7 @@ import com.example.order_service.client.ProductClient;
 import com.example.order_service.domin.Order;
 import com.example.order_service.dtos.ProductResponseDto;
 import com.example.order_service.repository.OrderRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +17,6 @@ public class OrderService {
 //    private final RestTemplate restTemp;
     @Autowired
     private ProductClient productClient;
-
     public OrderService(
             OrderRepository orderRepository,
             RestTemplate restTemp) {
@@ -24,6 +24,10 @@ public class OrderService {
 //        this.restTemp= restTemp;
     }
 
+    @CircuitBreaker(
+            name = "productService",
+            fallbackMethod = "productFallback"
+    )
     public Order createOrder(Order order){
 
        /* ProductResponseDto productResponseDto = restTemp.getForObject(
@@ -36,6 +40,15 @@ public class OrderService {
         order.setTotalPrice(productResponseDto.getPrice()* order.getQuantity());
 
         return orderRepository.save(order);
+    }
+
+    public Order productFallback(
+            Order order,
+            Exception ex
+    ){
+        throw new RuntimeException(
+                "Product Service is unavailable. Please try again later."
+        );
     }
 
 }
