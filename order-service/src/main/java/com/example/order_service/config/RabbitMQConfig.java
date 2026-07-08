d5d7877c018e0665aa2faa33892b9115349433f6
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-08 20:44:39 +0300

    feat: add JSON message converter for RabbitMQ events

--- order-service/src/main/java/com/example/order_service/config/RabbitMQConfig.java
+++ order-service/src/main/java/com/example/order_service/config/RabbitMQConfig.java
@@ -1,10 +1,15 @@
 package com.example.order_service.config;
 
 
+
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,5 +38,19 @@ public class RabbitMQConfig {
                 .bind(queue())
                 .to(exchange())
                 .with(ROUTING_KEY);
+    }
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(
+            ConnectionFactory connectionFactory,
+            MessageConverter messageConverter
+    ){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return  rabbitTemplate;
     }
 }
