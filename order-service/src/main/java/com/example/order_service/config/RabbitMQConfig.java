1f729d679e6664f8538c3942be165220fcc2d6f6
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-08 19:39:34 +0300

    return bean object of rabbitmq configuration

--- order-service/src/main/java/com/example/order_service/config/RabbitMQConfig.java
+++ order-service/src/main/java/com/example/order_service/config/RabbitMQConfig.java
@@ -1,5 +1,11 @@
 package com.example.order_service.config;
 
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,4 +16,22 @@ public class RabbitMQConfig {
     public static final String EXCHANGE = "order.exchange";
 
     public static final String ROUTING_KEY = "order.created";
+
+    @Bean
+    public Queue queue(){
+        return new Queue(QUEUE);
+    }
+
+    @Bean
+    public DirectExchange exchange(){
+        return new DirectExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(ROUTING_KEY);
+    }
 }
