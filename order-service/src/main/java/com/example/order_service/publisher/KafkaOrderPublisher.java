799b79d2a985335f50f1a48def72c90557ee4274
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-24 11:20:16 +0300

    update hardcoded kafka order topic to property based

--- order-service/src/main/java/com/example/order_service/publisher/KafkaOrderPublisher.java
+++ order-service/src/main/java/com/example/order_service/publisher/KafkaOrderPublisher.java
@@ -1,13 +1,15 @@
 package com.example.order_service.publisher;
 
 import com.example.order_service.event.OrderCreatedEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaOrderPublisher {
 
-    private static final String TOPIC = "order-events";
+    @Value("${app.kafka.order-topic}")
+    private String topic;
 
     private final KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate;
 
@@ -21,13 +23,13 @@ public class KafkaOrderPublisher {
             OrderCreatedEvent event){
 
         kafkaTemplate.send(
-                TOPIC,
+                topic,
                 event
         ).whenComplete(
                 (result,ex)->{
                     if(ex==null){
                         System.out.println(
-                                "Message Sent"
+                                "Message Sent"+ result.getRecordMetadata()
                         );
                     }
                     else{
