30bc6b99bce3d01cef20e4bc312924db788bb41d
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-27 19:25:45 +0300

    add Kafka DeadLetter instead of RabbitMQ

--- notification-service/src/main/java/com/example/notification_service/consumer/DeadLetterConsumer.java
+++ notification-service/src/main/java/com/example/notification_service/consumer/DeadLetterConsumer.java
@@ -3,9 +3,13 @@ package com.example.notification_service.consumer;
 //import com.example.notification_service.config.RabbitMQConfig;
 import com.example.notification_service.dto.OrderCreatedEvent;
 //import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class DeadLetterConsumer {
 
     /*@RabbitListener(
@@ -22,6 +26,17 @@ public class DeadLetterConsumer {
 
         );
 
+    }
+
+    @KafkaListener(
+            topics = "order-events.DLT",
+            groupId = "notification-group-dlt"
+    )
+    public void readDeadLetter(OrderCreatedEvent event, Acknowledgment acknowledgment) {
+
+        log.error("DLQ message received for orderId {} — needs manual attention", event.getOrderId());
+
+        acknowledgment.acknowledge();
     }
 
 }
