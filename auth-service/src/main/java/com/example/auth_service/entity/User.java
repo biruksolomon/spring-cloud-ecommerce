3e52d536a7306a5bd2165f80f4f04eb13b61b715
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-27 22:28:42 +0300

    adding user entity

---
+++ auth-service/src/main/java/com/example/auth_service/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.auth_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+    @Column(nullable = false, updatable = false)
+    private Long createdAt = System.currentTimeMillis();
+}
