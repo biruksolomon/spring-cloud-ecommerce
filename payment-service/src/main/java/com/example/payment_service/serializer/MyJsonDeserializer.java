85bfeef4bddb190554b61e25b8ec8a317f625081
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-28 12:00:02 +0300

    Custom Json Seserializer in payment service

---
+++ payment-service/src/main/java/com/example/payment_service/serializer/MyJsonDeserializer.java
@@ -0,0 +1,26 @@
+package com.example.payment_service.serializer;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+
+public class MyJsonDeserializer<T> implements Deserializer<T> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    private final Class<T> targetType;
+
+    public MyJsonDeserializer(Class<T> targetType) {
+        this.targetType = targetType;
+    }
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        try {
+            return data == null ? null : objectMapper.readValue(data, targetType);
+        } catch (Exception e) {
+            throw new SerializationException("Error deserializing JSON", e);
+        }
+    }
+}
