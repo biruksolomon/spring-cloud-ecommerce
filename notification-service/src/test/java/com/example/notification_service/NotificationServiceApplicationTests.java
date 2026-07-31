ff0b3ec8c03d128708242729d91b612bd3ccf74b
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-31 12:48:48 +0300

    test: mock test configuration for H2, Kafka, and service dependencies to pass placholder test ci

--- notification-service/src/test/java/com/example/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/example/notification_service/NotificationServiceApplicationTests.java
@@ -3,14 +3,11 @@ package com.example.notification_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest
 class NotificationServiceApplicationTests {
 
 	@Test
-	void True() {
-		assertTrue(true);
+	void contextLoads() {
 	}
 
 }
--- payment-service/src/test/java/com/example/payment_service/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/example/payment_service/PaymentServiceApplicationTests.java
@@ -3,13 +3,11 @@ package com.example.payment_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest
 class PaymentServiceApplicationTests {
 
 	@Test
-	void True() {
-		assertTrue(true);
+	void contextLoads() {
 	}
+
 }
