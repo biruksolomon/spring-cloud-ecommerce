6ca65f7e892305e62769b5ee47473518e79e11fd
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-08-14 13:03:40 +0300

    add(Refresh Token helper dtos): in auth service dto  basic Logout && Refresh req-res

---
+++ auth-service/src/main/java/com/example/auth_service/dto/LogoutRequest.java
@@ -0,0 +1,16 @@
+package com.example.auth_service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogoutRequest {
+    @NotBlank(message = "refreshToken is required")
+    private String refreshToken;
+}
--- auth-service/src/main/java/com/example/auth_service/dto/RefreshResponse.java
+++ auth-service/src/main/java/com/example/auth_service/dto/RefreshResponse.java
@@ -1,6 +1,5 @@
 package com.example.auth_service.dto;
 
-import com.example.auth_service.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,13 +9,8 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AuthResponse {
-    private Long userId;
-    private String email;
+public class RefreshResponse {
     private String token;
     private long expiresIn;
     private String refreshToken;
-    private String firstName;
-    private String lastName;
-    private Role role;
 }
