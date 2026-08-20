7adfffee8556b20f4d4d9c1bd780cf50ca467906
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-08-20 14:13:48 +0300

    add(config-service) Centeralized Spring Cloud Config server && remove old version config service factoring

--- config-server/src/main/java/com/example/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/config_server/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.config_service;
+package com.example.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServiceApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServiceApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
